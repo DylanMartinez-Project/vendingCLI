a9a7942a6a4001b3ce0b1bebf6321f74606ba283
Dylan Martinez <dylanmartinez.andre@gmail.com>
2021-03-06 19:47:57 -0500

    HW 1-5 2/4

--- module-1/05_Command_Line_Programs/exercise-student/java/src/main/java/com/techelevator/LinearConvert.java
+++ module-1/05_Command_Line_Programs/exercise-student/java/src/main/java/com/techelevator/LinearConvert.java
@@ -1,8 +1,35 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class LinearConvert {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println( "PLEASE ENTER THE LENGTH: " );
+
+		String lengthInput = scanner.nextLine();  //PERSON INPUT
+		double lengthNum = Double.parseDouble(lengthInput);  // NUM VALUE OF length
+
+		System.out.println("IS THE LENGTH IN (1)METERS or (2) FEET ? TYPE 1 OR 2");
+		String tempUnit = scanner.nextLine(); //PERSON INPUTS  UNIT
+
+		double length = Double.parseDouble(lengthInput);
+		double mOrFeet = Double.parseDouble(tempUnit);
+
+		if(mOrFeet == 1){
+			System.out.println(length + "m is "+ (length*3.280839)+ " f");
+// USES 1 TO CONVERT m TO f
+		}
+
+		else if(mOrFeet == 2){
+			System.out.println(length + "f is "+ (length*0.3048) + " m");
+		}
+		else {
+			// NOT 1 OR 2 ON TEMP UNIT INPUT
+			System.out.println("NOT A VALID INPUT. TYPE 1 OR 2");
+		}
+
 
 	}
 
--- module-1/05_Command_Line_Programs/exercise-student/java/src/main/java/com/techelevator/TempConvert.java
+++ module-1/05_Command_Line_Programs/exercise-student/java/src/main/java/com/techelevator/TempConvert.java
@@ -1,8 +1,36 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class TempConvert {
 
 	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println( "PLEASE ENTER THE TEMPERATURE: " );
+
+		String tempInput = scanner.nextLine();  //PERSON INPUT
+		double tempDegree = Double.parseDouble(tempInput);  // NUM VALUE OF DEGREE
+
+		System.out.println("IS THE TEMPERATURE IN (1)CELSIUS or (2) FAHRENHEIT? TYPE 1 OR 2");
+		String tempUnit = scanner.nextLine(); //PERSON INPUTS TEMP UNIT
+
+		double temperature = Double.parseDouble(tempInput);
+		double fahrOrCel = Double.parseDouble(tempUnit);
+
+		if(fahrOrCel == 1){
+			System.out.println(temperature + "C is "+ (temperature*1.8 + 32)+ "F");
+// USES 1 TO CONVERT C TO F
+		}
+
+		else if(fahrOrCel == 2){
+			System.out.println(temperature + "F is "+ (temperature-32)/1.8+ "C");
+		}
+		else {
+			// NOT 1 OR 2 ON TEMP UNIT INPUT
+			System.out.println("NOT A VALID INPUT. TYPE 1 OR 2");
+		}
+
 
 	}
 
