256cc5d0d6981d5e56bcbee42a9852c12c4756e2
Andy <achongs.business@gmail.com>
2021-03-23 12:10:36 -0400

    1-17 lecture final

---
+++ module-1/17_FileIO_Writing_out/lecture-final/java/src/main/java/com/techelevator/Appender.java
@@ -0,0 +1,36 @@
+package com.techelevator;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Appender {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write something meaningful.");
+        String lineFromUser = scanner.nextLine();
+
+        File file = new File("data.txt");
+
+        PrintWriter printWriter = null;
+        if (file.exists()) {
+
+            FileOutputStream outputStream = new FileOutputStream(file, true);
+            printWriter = new PrintWriter(outputStream);
+
+        } else {
+
+            printWriter = new PrintWriter(file);
+        }
+
+        printWriter.append(lineFromUser);
+        printWriter.flush();
+        printWriter.close();
+
+
+    }
+}
