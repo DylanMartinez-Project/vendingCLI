03f667de0e5ec6c7348d00e2d44cca0cd59d4037
Dylan Martinez <dylanmartinez.andre@gmail.com>
2021-04-14 12:53:50 -0400

    push

--- module-2/13_ServerSide_APIs_Part_1/lecture-student/server/java/src/main/java/com/techelevator/reservations/controllers/HotelController.java
+++ module-2/13_ServerSide_APIs_Part_1/lecture-student/server/java/src/main/java/com/techelevator/reservations/controllers/HotelController.java
@@ -1,6 +1,35 @@
 package com.techelevator.reservations.controllers;
+import java.util.List;
+import com.techelevator.reservations.HotelReservationsApplication;
+import com.techelevator.reservations.dao.HotelDAO;
+import com.techelevator.reservations.dao.MemoryHotelDAO;
+import com.techelevator.reservations.dao.MemoryReservationDAO;
+import com.techelevator.reservations.dao.ReservationDAO;
+import com.techelevator.reservations.models.Hotel;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
 public class HotelController {
 
+    private HotelDAO hotelDAO;
+    private ReservationDAO reservationDAO;
 
+    public HotelController() {
+        this.hotelDAO = new MemoryHotelDAO();
+        this.reservationDAO = new MemoryReservationDAO(hotelDAO);
+
+    }
+
+    @RequestMapping (path ="/hotels", method = RequestMethod.GET)
+    public List<Hotel> list(){
+
+        List<Hotel> hotels = hotelDAO.list();
+        return hotels;
+    }
 }
+
+
