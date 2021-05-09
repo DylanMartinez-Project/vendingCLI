d9263d5c64bfbf123243384990e426d91c35ae1d
Andy Chong Sam <andy@techelevator.com>
2021-05-09 16:38:21 -0400

    3-11 partial

---
+++ module-3/11_Introduction_to_Vue_and_Data_Binding/lecture-student/src/components/ProductReview.vue
@@ -0,0 +1,183 @@
+<template>
+<!--Beginning of HTML Section-->
+  <div class="main">
+    <h2>Product Reviews for {{ name }}</h2>
+
+    <p class="description"></p>
+
+    <div class="well-display">
+      <div class="well">
+        <span class="amount"></span>
+        Average Rating
+      </div>
+
+      <div class="well">
+        <span class="amount"></span>
+        1 Star Review
+      </div>
+
+      <div class="well">
+        <span class="amount"></span>
+        2 Star Review
+      </div>
+
+      <div class="well">
+        <span class="amount"></span>
+        3 Star Review
+      </div>
+
+      <div class="well">
+        <span class="amount"></span>
+        4 Star Review
+      </div>
+
+      <div class="well">
+        <span class="amount"></span>
+        5 Star Review
+      </div>
+    </div>
+
+    <div
+      class="review"
+    >
+      <!--Loop through review data-->
+    </div>
+  </div>
+</template>
+
+<script>
+// Beginning of JavaScript section.
+export default {
+  name: "product-review",
+  data() {
+    return {
+      name: "Cigar Parties for Dummies",
+      description:
+        "Host and plan the perfect cigar party for all of your squirrelly friends.",
+      reviews: [
+        {
+          reviewer: "Malcolm Gladwell",
+          title: "What a book!",
+          review:
+            "It certainly is a book. I mean, I can see that. Pages kept together with glue and there's writing on it, in some language.",
+          rating: 3,
+          favorited: false
+        },
+        {
+          reviewer: "Tim Ferriss",
+          title: "Had a cigar party started in less than 4 hours.",
+          review:
+            "It should have been called the four hour cigar party. That's amazing. I have a new idea for muse because of this.",
+          rating: 4,
+          favorited: false
+        },
+        {
+          reviewer: "Ramit Sethi",
+          title: "What every new entrepreneurs needs. A door stop.",
+          review:
+            "When I sell my courses, I'm always telling people that if a book costs less than $20, they should just buy it. If they only learn one thing from it, it was worth it. Wish I learned something from this book.",
+          rating: 1,
+          favorited: false
+        },
+        {
+          reviewer: "Gary Vaynerchuk",
+          title: "And I thought I could write",
+          review:
+            "There are a lot of good, solid tips in this book. I don't want to ruin it, but prelighting all the cigars is worth the price of admission alone.",
+          rating: 3,
+          favorited: false
+        }
+      ]
+    };
+  },
+  computed: {
+    averageRating() {
+
+      return 0;
+
+    },
+    numberOfOneStarReviews() {
+
+      return 0;
+    },
+    numberOfTwoStarReviews() {
+
+      return 0;
+    },
+    numberOfThreeStarReviews() {
+
+      return 0;
+    },
+    numberOfFourStarReviews() {
+
+      return 0;
+    },
+    numberOfFiveStarReviews() {
+
+      return 0;
+    }
+  }
+};
+</script>
+
+<style scoped>
+/** Beginning of CSS Section */
+div.main {
+  margin: 1rem 0;
+}
+
+div.main div.well-display {
+  display: flex;
+  justify-content: space-around;
+}
+
+div.main div.well-display div.well {
+  display: inline-block;
+  width: 15%;
+  border: 1px black solid;
+  border-radius: 6px;
+  text-align: center;
+  margin: 0.25rem;
+}
+
+div.main div.well-display div.well span.amount {
+  color: darkslategray;
+  display: block;
+  font-size: 2.5rem;
+}
+
+div.main div.review {
+  border: 1px black solid;
+  border-radius: 6px;
+  padding: 1rem;
+  margin: 10px;
+}
+
+div.main div.review.favorited {
+  background-color: lightyellow;
+}
+
+div.main div.review div.rating {
+  height: 2rem;
+  display: inline-block;
+  vertical-align: top;
+  margin: 0 0.5rem;
+}
+
+div.main div.review div.rating img {
+  height: 100%;
+}
+
+div.main div.review p {
+  margin: 20px;
+}
+
+div.main div.review h3 {
+  display: inline-block;
+}
+
+div.main div.review h4 {
+  font-size: 1rem;
+}
+</style>
+
