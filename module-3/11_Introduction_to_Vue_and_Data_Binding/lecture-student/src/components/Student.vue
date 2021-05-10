409e52efdaa59669794dbb195ab812c6479bf998
Andy Chong Sam <andy@techelevator.com>
2021-05-10 12:15:15 -0400

    3-11 lecture final

---
+++ module-3/11_Introduction_to_Vue_and_Data_Binding/lecture-student/src/components/Student.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <p>My name is: {{name}}. </p>
+        <ul>
+            <li v-for='skill in stack' v-bind:key='skill' class='li-items'>{{skill}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Student',
+    data() {
+        return {
+            name: 'Andy',
+            stack: ['Java', 'SQL', 'CSS', 'HTML', 'JS', 'VUE']
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+    .li-items {
+        text-align: left;
+    }
+</style>
