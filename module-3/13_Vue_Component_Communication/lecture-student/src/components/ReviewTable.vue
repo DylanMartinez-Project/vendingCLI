6f57edf6705685d41ac076889286ea1c920556f7
Andy Chong Sam <andy@techelevator.com>
2021-05-13 07:30:33 -0400

    Pushing 3-13

---
+++ module-3/13_Vue_Component_Communication/lecture-student/src/components/ReviewTable.vue
@@ -0,0 +1,54 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>Title</th>
+        <th>Reviewer</th>
+        <th>Review</th>
+        <th>Rating</th>
+        <th>Favorited</th>
+      </tr>
+    </thead>
+    <tbody>
+      <review-table-row
+        v-for="review in filteredReviews"
+        v-bind:key="review.title"
+        v-bind:review="review"
+      />
+      <tr v-show="filteredReviews.length == 0">
+        <td colspan="5">There are no reviews</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import ReviewTableRow from "./ReviewTableRow";
+
+export default {
+  name: "review-table",
+  components: {
+    ReviewTableRow
+  },
+  computed: {
+    filteredReviews() {
+      // retrieve and filter from state.
+        return [];
+    }
+  }
+};
+</script>
+
+<style style="scoped">
+th,
+td {
+  text-align: left;
+}
+td {
+  padding-right: 10px;
+  vertical-align: top;
+}
+tr:nth-child(even) {
+  background-color: rgb(238, 238, 238);
+}
+</style>
