7f1c70f3706fc8452efdf80355aae7e4a14b3b6e
Andy Chong Sam <andy@techelevator.com>
2021-05-18 08:12:21 -0400

    pushing 3-15

---
+++ module-3/15_Vue_Routing/lecture-student/src/views/Products.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="products">
+    <h1>Products</h1>
+
+  </div>
+</template>
+
+<script>
+// include the ProductsList component
+
+export default {
+  components: {
+
+  }
+};
+</script>
